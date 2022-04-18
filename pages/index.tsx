a5e31605fe996447041a181c5b63bb79d6605d14
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-04-18 11:52:45 -0400

    add MUI and update index page route

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,54 @@
-import Gallery from '../components/Gallery'
+import { useEffect, useState } from "react";
+import {
+  createApplication,
+  deleteApplication,
+  getAllApplications,
+  getSingleApplication,
+  updateApplication,
+} from "../utils/api";
+import { ApplicationData } from "types/Application";
+import { Button } from "@mui/material";
+import { Head } from "components/Head";
 
-const Index = () => <Gallery />
+const Tracker = () => {
+  const [data, setData] = useState<ApplicationData[]>([]);
+  useEffect(() => {
+    getAllApplications().then((res) => setData(res.data));
 
-export default Index
+    getSingleApplication(1).then((res) => console.log(res.data));
+    createApplication({
+      companyName: "ABC",
+      appliedOn: new Date(),
+      website: "abc.co",
+      interview: false,
+      notes: "is this thing on?",
+    });
+    updateApplication(1, {
+      companyName: "ACME UPDATED",
+      website: "some-site",
+      interview: true,
+      appliedOn: new Date(),
+      notes: "updated!",
+    });
+  }, []);
+  return (
+    <>
+      <Head title="Job Applications" />
+      <h1>Job Applications</h1>
+      <ul>
+        {data.map((d, index) => (
+          <>
+            <li key={index + "-application-data"}>
+              {d.companyName ? d.companyName : "NA"}
+            </li>
+            <Button variant="contained" onClick={() => deleteApplication(d.id)}>
+              X
+            </Button>
+          </>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Tracker;
